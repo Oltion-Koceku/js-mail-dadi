81f384f778d3bbb5e9bf0ec9a0317eedab896519
Oltion-Koceku <koceku17@gmail.com>
2024-02-23 16:10:51 +0100

    [end]

--- js/script.js
+++ js/script.js
@@ -1,27 +1,63 @@
-// let emailUtente = prompt("Scrivi la tua email.")
-// const emailRegistrate = ["q@gmail.com, w@gmail.com, e@gmail.com, r@gmail.com, t@gmail.com, y@gmail.com,"]
-// console.log(emailUtente, emailRegistrate.toString());
-// if(emailUtente = emailRegistrate.at(0, 1, 2, 3, 4, 5).toString()){
-//   console.log("corretto");
-// }else{
-//   document.querySelector("h1").innerHTML += 
-//   `
-//   Hai sbagliato email oppure non ti sei ancora registrato.
-//   `
-// }
+// mail
+const emailUtente = [prompt("Scrivi la tua email.")];
+const emailRegistrate = ["q@gmail.com", "w@gmail.com", "e@gmail.com", "r@gmail.com", "t@gmail.com", "y@gmail.com",] 
 
+console.log(emailUtente, emailRegistrate);
 
-// Dadi
+if(emailUtente[0] === emailRegistrate[0]){
+  console.log("correwtto");
+  console.log(emailRegistrate.at(0, 1 ,2 ,3));
+}else if(emailUtente[0] === emailRegistrate[1]){
+  console.log("correwtto");
 
-const utente = Math.floor(Math.random() * 6) + 1
-const computer = Math.floor(Math.random() * 6) + 1
-console.log(utente, computer);
+}else if(emailUtente[0] === emailRegistrate[2]){
+  console.log("correwtto");
 
-if(utente > computer){
-  console.log("vince l'utente");
-}else if(computer > utente){
-  console.log("vince il computer");
+}else if(emailUtente[0] === emailRegistrate[3]){
+  console.log("correwtto");
+
+}else if(emailUtente[0] === emailRegistrate[4]){
+  console.log("correwtto");
+
+}else if(emailUtente[0] === emailRegistrate[5]){
+  console.log("correwtto");
 
 }else{
-console.log("riprova");
+  console.log("SBAGLIATO");
+  document.querySelector("h1").innerHTML += 
+  `
+  Hai sbagliato email oppure non ti sei ancora registrato.
+  `
 }
+// Dadi
+const bottone = document.querySelector(".btn");
+bottone.addEventListener("click", function(){
+  const utente = Math.floor(Math.random() * 6) + 1;
+  const computer = Math.floor(Math.random() * 6) + 1;
+  console.log(utente, computer);
+  
+  if(utente > computer){
+    console.log("vince l'utente");
+    document.querySelector("h2").innerHTML +=
+    `
+    vince l'utente con ${utente} rispetto a ${computer} del computer
+    `;
+    
+    
+  }else if(computer > utente){
+    console.log("vince il computer");
+    document.querySelector("h2").innerHTML +=
+    `
+    vince il computer con ${computer} rispetto a ${utente} dell'utente
+    `
+  }else{
+  console.log("riprova");
+  document.querySelector("h2").innerHTML +=
+    `
+    Pareggio Ritenta schiacciando di nuovo il pulsante
+    `
+  }
+
+})
+
+
